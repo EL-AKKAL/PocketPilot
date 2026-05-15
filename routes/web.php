7318cd003b242ae7886ddd44629cf75ae5694230
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-05-16 00:18:03 +0100

    remove unused props in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PeriodicTransactionController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified', 'account'])->group(function () {
 
