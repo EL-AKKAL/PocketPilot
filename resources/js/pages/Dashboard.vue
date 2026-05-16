ba0c433e3086d05001a1be4dd274c6743463d174
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-05-16 22:10:59 +0100

    fix dashboard darkmode

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -11,27 +11,27 @@ defineProps({
 <template>
     <div class="space-y-6 p-6">
         <!-- Balance -->
-        <div class="rounded-2xl bg-white p-6 shadow">
-            <h2 class="text-gray-500">Total Balance</h2>
+        <div class="rounded-2xl border p-6 shadow">
+            <h2 class="text-primary">Total Balance</h2>
             <p class="mt-2 text-3xl font-bold">{{ balance?.toFixed(2) }}MAD</p>
         </div>
 
         <!-- Income vs Expense -->
         <div class="grid grid-cols-2 gap-4">
-            <div class="rounded-xl bg-green-100 p-4">
+            <div class="rounded-xl border p-4">
                 <h3 class="text-green-700">Income</h3>
                 <p class="text-xl font-bold">{{ income?.toFixed(2) }}MAD</p>
             </div>
 
-            <div class="rounded-xl bg-red-100 p-4">
+            <div class="rounded-xl border p-4">
                 <h3 class="text-red-700">Expense</h3>
                 <p class="text-xl font-bold">{{ expense?.toFixed(2) }}MAD</p>
             </div>
         </div>
 
         <!-- Recent Transactions -->
-        <div class="rounded-2xl bg-white p-6 shadow">
-            <h2 class="mb-4 font-semibold">Recent Transactions</h2>
+        <div class="rounded-2xl border bg-accent/20 p-6 shadow">
+            <h2 class="mb-4 font-semibold text-primary">Recent Transactions</h2>
 
             <div v-if="recentTransactions?.length === 0">
                 No transactions yet.
