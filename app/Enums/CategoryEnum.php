bf4ca3ae09049a6b332e49b9ecfa9da410d4abad
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-05-26 18:30:50 +0100

    add category enum

---
+++ app/Enums/CategoryEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum CategoryEnum: string
+{
+    case FOOD = 'Food';
+    case TRANSPORT = 'Transport';
+    case BILLS = 'Bills';
+    case NEEDS = 'Needs';
+    case WANTS = 'Wants';
+    case EMERGENCIES = 'Emergencies';
+    case FIXES = 'Fixes';
+
+    public static function values(): array
+    {
+        return array_map(fn ($case) => $case->value, self::cases());
+    }
+}
