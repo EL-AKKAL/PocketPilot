d3ceb4096ee01460ac8cf86ab7804acec384fdd8
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-06-04 01:17:45 +0100

    add categories table

---
+++ database/migrations/2026_06_03_231458_create_categories_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('value');
+            $table->string('description')->nullable();
+
+            $table->enum('type', [
+                'income',
+                'expense',
+            ]);
+
+            $table->timestamps();
+
+            Schema::table('transactions', function (Blueprint $table) {
+                $table->foreignId('category_id')
+                    ->nullable()
+                    ->after('account_id')
+                    ->constrained()
+                    ->nullOnDelete();
+            });
+
+            Schema::table('periodic_transactions', function (Blueprint $table) {
+                $table->foreignId('category_id')
+                    ->nullable()
+                    ->after('account_id')
+                    ->constrained()
+                    ->nullOnDelete();
+            });
+
+            $table->unique(['account_id', 'value']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('category_id');
+        });
+
+        Schema::table('periodic_transactions', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('category_id');
+        });
+
+        Schema::dropIfExists('categories');
+    }
+};
