5704138b59b93e0e33a2f512f894ced6da1fa452
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-06-21 20:33:05 +0100

    feat: add debt routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DebtController;
 use App\Http\Controllers\GoalController;
 use App\Http\Controllers\PeriodicTransactionController;
 use App\Http\Controllers\TransactionController;
@@ -13,10 +14,11 @@ Route::middleware(['auth', 'verified', 'account'])->group(function () {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::resource('transactions', TransactionController::class)->except('show', 'edit');
-    Route::resource('periodic_transactions', PeriodicTransactionController::class)->except('show', 'edit');
+    Route::resource('transactions', TransactionController::class)->except('show', 'edit', 'create');
+    Route::resource('periodic_transactions', PeriodicTransactionController::class)->except('show', 'edit', 'create');
     Route::resource('categories', CategoryController::class)->except('show', 'create', 'edit');
     Route::resource('goals', GoalController::class)->except('show', 'create', 'edit', 'delete');
+    Route::resource('debts', DebtController::class)->except('show', 'create', 'edit');
 });
 
 require __DIR__.'/settings.php';
